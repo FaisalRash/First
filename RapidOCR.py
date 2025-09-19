52a226b84733fa6c831882c42f49be184cb07f75
EMp <faisal.rash99@gmail.com>
2025-09-19 17:30:51 +0530

    First file addition

---
+++ RapidOCR.py
@@ -0,0 +1,57 @@
+# pip install docling
+# pip install rapidocr_onnxruntime
+
+import os
+from huggingface_hub import snapshot_download
+from docling.datamodel.pipeline_options import PdfPipelineOptions, RapidOcrOptions
+from docling.document_converter import (
+    ConversionResult,
+    DocumentConverter,
+    InputFormat,
+    PdfFormatOption,
+    ImageFormatOption,
+)
+def RapidOCR(path):
+    # Source document to convert
+    source = path
+
+    # Download RappidOCR models from HuggingFace
+    print("Downloading RapidOCR models")
+    download_path = snapshot_download(repo_id="SWHL/RapidOCR")
+
+    # Setup RapidOcrOptions for english detection
+    det_model_path = os.path.join(
+        download_path, "PP-OCRv4", "en_PP-OCRv3_det_infer.onnx"
+    )
+    rec_model_path = os.path.join(
+        download_path, "PP-OCRv4", "ch_PP-OCRv4_rec_server_infer.onnx"
+    )
+    cls_model_path = os.path.join(
+        download_path, "PP-OCRv3", "ch_ppocr_mobile_v2.0_cls_train.onnx"
+    )
+    ocr_options = RapidOcrOptions(
+        det_model_path=det_model_path,
+        rec_model_path=rec_model_path,
+        cls_model_path=cls_model_path,
+    )
+
+    pipeline_options = PdfPipelineOptions(
+        ocr_options=ocr_options,
+    )
+
+    # Convert the document
+    converter = DocumentConverter(
+        format_options={
+            # InputFormat.PDF: PdfFormatOption(
+            InputFormat.IMAGE: ImageFormatOption(
+                pipeline_options=pipeline_options,
+            ),
+        },
+    )
+
+    conversion_result: ConversionResult = converter.convert(source=source)
+    doc = conversion_result.document
+    md = doc.export_to_markdown()
+    print(md)
+
+RapidOCR(r"C:\Users\NX542NJ\Downloads\Coordinate Delivery Based Outbound Transportation Planning.pdf")
